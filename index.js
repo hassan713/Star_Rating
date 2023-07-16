656fec49eb82e059683243cf7d863dc87f26b5fc
hassan <hasanxax713@gmail.com>
2023-07-16 21:59:06 +0200

    first commit develope

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
+
 const stars = document.querySelectorAll(".stars i"); 
-// console.log(stars);
 
-stars.forEach((star,indexA) => {
-	star.addEventListener("click", () => {
-		console.log("click");
-		console.log(star);
+// stars.forEach(function (elements,indexA){
+// 	// console.log("etoile :" + indexA);
+// 	elements.addEventListener("click", function () {
+// 		// console.log("click");
+// 		// console.log(elements);
+// 		// console.log(indexA);
+
+// 		stars.forEach(function (elements, indexB){
+// 			// console.log("click");
+// 			// console.log(elements);
+// 			// console.log(indexB);			
+// 			if(indexA >= indexB){
+// 				elements.classList.add("active")
+// 			}else{
+// 				elements.classList.remove("active")
+// 			}
+
+// 		})
+// 	})
+// })
+
+
+
+stars.forEach(myfunctionA);
+
+function myfunctionA(elements, indexA){
+	console.log("etoile :" + indexA);
+	elements.addEventListener("click", () => {
 		console.log(indexA);
+		console.log("etoile :" + indexA);
+	stars.forEach(myfunctionB);
+		function myfunctionB (elements, indexB) {
 
-		stars.forEach((star, indexB) => {
-			// console.log("click");
-			// console.log(star);
-			// console.log(indexB);			
-			if(indexA >= indexB){
-				star.classList.add("active")
-			}else{
-				star.classList.remove("active")
-			}
+			console.log(indexB);			
+				if(indexA >= indexB){
+					elements.classList.add("active")
+				}else{
+					elements.classList.remove("active")
+				}
 
-		})
+		}
 	})
-})
+}
